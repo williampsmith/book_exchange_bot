d45fa6a23b3f4d9438ede7e7cf97d83783e95acd
William Smith <williampsmith@fb.com>
2020-07-12 17:42:14 -0700

    use correct prefix

--- index.js
+++ index.js
@@ -24,29 +24,29 @@ bot.on('message', message => {
     const command = args.shift().toLowerCase();
 
     switch(command) {
-        case '!bookbot':
+        case 'bookbot':
             message.reply(
                 "Hi, I'm the Book Exchange Bot :robot:! I can help you manage book exchanges. \n" +
                 "Here are some ways you can interact with me:\n" +
-                "!bookbot  :point_right:  Show this message\n" +
-                "!newbook <book name, optionally with author>  :point_right:  update your shelf list with the given book and post link\n" +
-                "!reading <book name, optionally with author>  :point_right:  update your reading list with the given book and post link\n" +
-                "!justread <book name, optionally with author>  :point_right:  update your read list with the given book and post link\n" +
-                "!viewshelf  :point_right:  View all available books with owner and status (reading, lent, read)\n" +
-                "!viewshelf @user  :point_right:  View the bookshelf of the given user\n" +
-                "!loan <book name, optionally with author> @user  :point_right:  Update your loaned list to reflect that the given book " +
+                "!!bookbot  :point_right:  Show this message\n" +
+                "!!newbook <book name, optionally with author>  :point_right:  update your shelf list with the given book and post link\n" +
+                "!!reading <book name, optionally with author>  :point_right:  update your reading list with the given book and post link\n" +
+                "!!justread <book name, optionally with author>  :point_right:  update your read list with the given book and post link\n" +
+                "!!viewshelf  :point_right:  View all available books with owner and status (reading, lent, read)\n" +
+                "!!viewshelf @user  :point_right:  View the bookshelf of the given user\n" +
+                "!!loan <book name, optionally with author> @user  :point_right:  Update your loaned list to reflect that the given book " +
                 "was lent to the given user"
             );
             break;
-        case '!newbook':
+        case 'newbook':
             break;
-        case '!reading':
+        case 'reading':
             break;
-        case '!justread':
+        case 'justread':
             break;
-        case '!viewshelf':
+        case 'viewshelf':
             break;
-        case '!loan':
+        case 'loan':
             break;
         default:
             message.channel.send('This command is unknown. To see supported commands, enter !!bookbot.');
